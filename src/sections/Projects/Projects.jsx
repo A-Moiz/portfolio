665608e1ad42502e3e8fce93a77568af15346b71
Abdul Moiz <abdul.m01z10123@gmail.com>
2024-10-28 20:47:14 +0000

    Added new project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import styles from "./ProjectsStyles.module.css";
 import bmiBuddy from "../../assets/bmiBuddy.png";
+import vocabio from "../../assets/vocabio.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -12,6 +13,13 @@ function Projects() {
           link={"https://apps.apple.com/gb/app/bmi-buddy/id6683310423"}
           h3="BMI Buddy"
           p='Track & Manage Your BMI Easily'
+        />
+
+        <ProjectCard
+          src={vocabio}
+          link={"https://apps.apple.com/gb/app/vocabio/id6737419951"}
+          h3="Vocabio"
+          p="Decode Today's Slang"
         />
       </div>
     </section>
