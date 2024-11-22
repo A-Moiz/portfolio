df9cb76ba7f5bfd376cd1a6632ecd3b808a3feaa
Abdul Moiz <abdul.m01z10123@gmail.com>
2024-11-22 01:59:44 +0000

    Added new CV

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,7 +6,8 @@ import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
-import CV from '../../assets/cv.pdf';
+import CV from '../../assets/CV-2.pdf';
+// import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
