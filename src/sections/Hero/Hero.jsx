20ae96271b966438a4ecfa05f62f75ce34e6cf7d
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2025-09-10 22:03:17 +0100

    Fixed issue related to CV

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -7,9 +7,9 @@ import linkedinDark from "../../assets/linkedin-dark.svg";
 import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import { useTheme } from "../../common/ThemeContext";
-import CV from "../../assets/abdul-moiz-cv.pdf";
 import referenceLetter from "../../assets/reference-letter.pdf";
 import awardConfirmation from "../../assets/award-confirmation.pdf";
+import CV from "../../assets/abdul-moiz-cv.pdf";
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
